1632696f523cb38e023b9259d6bd16025f146256
DianaBych <dianavika18@gmail.com>
2026-02-08 01:46:07 +0300

    fix: upgrade task2 and task5

--- task2.ts
+++ task2.ts
@@ -4,7 +4,7 @@ const word1 = "Анна";
 const word2 = "Кабак";
 const word3 = "Кот";
 function checkPalindrome(word: string) {
-  return word.split("").reverse().join("") === word.toLowerCase();
+  return word.toLowerCase().split("").reverse().join("") === word.toLowerCase();
 }
 console.log(checkPalindrome(word1));
 console.log(checkPalindrome(word2));
--- task5.ts
+++ task5.ts
@@ -3,4 +3,14 @@
 
 const array = [1, [2, 3], [4], 5, [6, 7, 8]];
 
-const result = [1, 2, 3, 4, 5, 6, 7, 8];
+function transformArray(arr: (number | number[])[]): number[] {
+  const strArray = array.join();
+  const strResult = strArray.split(",");
+  const result = [];
+  for (let i = 0; i < strResult.length; i++) {
+    result.push(Number(strResult[i]));
+  }
+  return result;
+}
+
+console.log(transformArray(array));
